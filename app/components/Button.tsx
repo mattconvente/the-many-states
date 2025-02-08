cfc651d73c24aa199d08a86445280d755f7ac70e
Matt Convente <matt@mattconvente.com>
2025-02-08 16:10:50 -0500

    Add social sharing

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,28 +2,40 @@
 
 import React from "react";
 import clsx from "clsx";
-
-type ButtonVariants = "primary" | "secondary" | "facebook" | "x";
+import {
+  ctaCoreClasses,
+  ctaSizeDefaultClasses,
+  ctaSizeSquareClasses,
+  ctaVariantPrimaryClasses,
+  ctaVariantSecondaryClasses,
+  ctaVariantFacebookClasses,
+  ctaVariantXClasses,
+} from "@/app/components/sharedClasses";
+import { CtaSizes, CtaVariants } from "@/app/types";
 
 interface ButtonProps {
   children: React.ReactNode;
-  className: string;
+  className?: string;
   onClick: () => void;
-  variant?: ButtonVariants;
+  size?: CtaSizes;
+  variant?: CtaVariants;
 }
 
 export default function Button({
   children,
   className,
   onClick,
+  size = "default",
   variant = "primary",
   ...rest
 }: ButtonProps) {
-  const coreButtonClasses = "transition inline-flex justify-center items-center gap-3 cursor-pointer font-bold py-2.5 px-8 lg:py-4 lg:px-12 text-sm lg:text-lg rounded-md lg:rounded-lg outline-hidden outline-2 outline-offset-2 outline-transparent";
-  const buttonClasses = clsx(coreButtonClasses, {
-    "text-white bg-linear-to-r from-(--color-old-glory-red) to-(--color-old-glory-blue) hover:outline-(--color-old-glory-blue) focus-visible:outline-(--color-old-glory-blue)": variant === "primary",
-    "bg-(--color-state-border-resting) hover:outline-foreground focus-visible:outline-foreground": variant === "secondary",
-    "text-white": variant === "facebook" || variant === "x",
+  const buttonClasses = clsx(ctaCoreClasses, {
+    [`${ctaSizeDefaultClasses}`]: size === "default",
+    [`${ctaSizeSquareClasses}`]: size === "square",
+    [`${ctaVariantPrimaryClasses}`]: variant === "primary",
+    [`${ctaVariantSecondaryClasses}`]: variant === "secondary",
+    [`${ctaVariantFacebookClasses}`]: variant === "facebook",
+    [`${ctaVariantXClasses}`]: variant === "x",
   }, className);
 
   return (
---
+++ app/components/sharedClasses.ts
@@ -0,0 +1,17 @@
+const ctaCoreClasses = "transition inline-flex justify-center items-center gap-3 font-bold cursor-pointer outline-hidden outline-2 outline-offset-2 outline-transparent";
+const ctaSizeDefaultClasses = "py-2.5 px-8 lg:py-4 lg:px-12 text-sm lg:text-lg rounded-md lg:rounded-lg";
+const ctaSizeSquareClasses = "p-2 size-10 rounded-lg";
+const ctaVariantPrimaryClasses = "text-white bg-linear-to-r from-(--color-old-glory-red) to-(--color-old-glory-blue) hover:outline-(--color-old-glory-blue) focus-visible:outline-(--color-old-glory-blue)";
+const ctaVariantSecondaryClasses = "bg-(--color-state-border-resting) hover:outline-foreground focus-visible:outline-foreground";
+const ctaVariantFacebookClasses = "text-white bg-(--color-facebook-bg)  hover:outline-(--color-facebook-bg) focus-visible:outline-(--color-facebook-bg)";
+const ctaVariantXClasses = "text-white bg-(--color-x-bg) hover:outline-(--color-x-bg) focus-visible:outline-(--color-x-bg)";
+
+export {
+  ctaCoreClasses,
+  ctaSizeDefaultClasses,
+  ctaSizeSquareClasses,
+  ctaVariantPrimaryClasses,
+  ctaVariantSecondaryClasses,
+  ctaVariantFacebookClasses,
+  ctaVariantXClasses,
+};
