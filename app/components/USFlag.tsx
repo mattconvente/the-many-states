6503e57611cbcfb2059fd1ada6ddabbc2cd5fdbb
Matt Convente <matt@mattconvente.com>
2025-02-23 18:22:18 -0500

    Set Flag opacity to 60% if no visitedStates.

--- app/components/USFlag.tsx
+++ app/components/USFlag.tsx
@@ -15,7 +15,10 @@ export default function USFlag({ className, visitedStates = [] }: USFlagProps) {
   const hoveredVisitedState = useTheManyStatesStore((state) => state.hoveredVisitedState);
   const hoveredUnvisitedState = useTheManyStatesStore((state) => state.hoveredUnvisitedState);
   const selectedStateAbbrs = visitedStates.map((state: IState) => state.abbr);
-  const usFlagClasses = clsx("flag-container w-full md:mx-auto md:max-w-3xl lg:sticky lg:max-w-full lg:top-12", className);
+  const usFlagClasses = clsx("flag-container w-full md:mx-auto md:max-w-3xl lg:sticky lg:max-w-full lg:top-12",
+    {
+      "opacity-60": visitedStates.length === 0,
+    }, className);
 
   return (
     <div className={usFlagClasses}>
